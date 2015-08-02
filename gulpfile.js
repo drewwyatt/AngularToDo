b2d9470dff8ec9aa3b7c1147d1d8a16ee6885fae
Drew Wyatt <drew.j.wyatt@gmail.com>
2015-08-02 17:59:20 -0500

    added auto compilation through gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
+var watch = require('gulp-watch');
 
-gulp.task('default', function() {
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.ts', ['compile-typescript']);
+});
+
+gulp.task('compile-typescript', function() {
   var tsResult = gulp.src('src/**/*.ts')
     .pipe(ts({
         noImplicitAny: true,
